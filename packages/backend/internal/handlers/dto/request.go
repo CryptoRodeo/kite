b31ce74a0ebfb692b8437d2b34a6fb82f7eb55fb
Bryan Ramos <bramos@redhat.com>
2025-08-07 16:19:27 -0400

    Create interface for scope data
    We need this data required for CREATE, optional for UPDATE.
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- packages/backend/internal/handlers/dto/request.go
+++ packages/backend/internal/handlers/dto/request.go
@@ -9,12 +9,42 @@ import (
 // DTOs (Data Transfer Objects)
 // These allow us to carry and format data between layers or services, without embedding any business logic.
 
-// For requests
+// This interface allows the scope payload to be used interchangeably
+// between CREATE and UPDATE requests.
+//
+// This is important because for CREATE we want the fields required,
+// but for UPDATE we want them optional.
+type ScopePayload interface {
+	GetResourceType() string
+	GetResourceName() string
+	GetResourceNamespace() string
+	AsOptional() ScopeReqBodyOptional
+}
 
 type ScopeReqBody struct {
 	ResourceType      string `json:"resourceType" binding:"required"`
 	ResourceName      string `json:"resourceName" binding:"required"`
 	ResourceNamespace string `json:"resourceNamespace"`
+}
+
+func (s ScopeReqBody) GetResourceType() string      { return s.ResourceType }
+func (s ScopeReqBody) GetResourceName() string      { return s.ResourceName }
+func (s ScopeReqBody) GetResourceNamespace() string { return s.ResourceNamespace }
+func (s ScopeReqBody) AsOptional() ScopeReqBodyOptional {
+	return ScopeReqBodyOptional(s)
+}
+
+type ScopeReqBodyOptional struct {
+	ResourceType      string `json:"resourceType"`
+	ResourceName      string `json:"resourceName"`
+	ResourceNamespace string `json:"resourceNamespace"`
+}
+
+func (s ScopeReqBodyOptional) GetResourceType() string      { return s.ResourceType }
+func (s ScopeReqBodyOptional) GetResourceName() string      { return s.ResourceName }
+func (s ScopeReqBodyOptional) GetResourceNamespace() string { return s.ResourceNamespace }
+func (s ScopeReqBodyOptional) AsOptional() ScopeReqBodyOptional {
+	return s
 }
 
 type CreateIssueRequest struct {
@@ -34,15 +64,15 @@ type CreateLinkRequest struct {
 }
 
 type UpdateIssueRequest struct {
-	Title       string              `json:"title"`
-	Description string              `json:"description"`
-	Severity    models.Severity     `json:"severity"`
-	IssueType   models.IssueType    `json:"issueType"`
-	State       models.IssueState   `json:"state"`
-	Namespace   string              `json:"namespace"`
-	Scope       ScopeReqBody        `json:"scope"`
-	Links       []CreateLinkRequest `json:"links"`
-	ResolvedAt  time.Time           `json:"resolvedAt"`
+	Title       string               `json:"title"`
+	Description string               `json:"description"`
+	Severity    models.Severity      `json:"severity"`
+	IssueType   models.IssueType     `json:"issueType"`
+	State       models.IssueState    `json:"state"`
+	Namespace   string               `json:"namespace"`
+	Scope       ScopeReqBodyOptional `json:"scope"`
+	Links       []CreateLinkRequest  `json:"links"`
+	ResolvedAt  time.Time            `json:"resolvedAt"`
 }
 
 // This interface allows for both create and update request structs
@@ -56,7 +86,7 @@ type IssuePayload interface {
 	GetLinks() []CreateLinkRequest
 	GetResolvedAt() time.Time
 	GetNamespace() string
-	GetScope() ScopeReqBody
+	GetScope() ScopePayload
 }
 
 func (c CreateIssueRequest) GetTitle() string               { return c.Title }
@@ -65,12 +95,13 @@ func (c CreateIssueRequest) GetSeverity() models.Severity   { return c.Severity 
 func (c CreateIssueRequest) GetIssueType() models.IssueType { return c.IssueType }
 func (c CreateIssueRequest) GetState() models.IssueState    { return c.State }
 func (c CreateIssueRequest) GetLinks() []CreateLinkRequest  { return c.Links }
-func (c CreateIssueRequest) GetScope() ScopeReqBody         { return c.Scope }
+func (c CreateIssueRequest) GetScope() ScopePayload         { return c.Scope }
 func (c CreateIssueRequest) GetNamespace() string           { return c.Namespace }
-
-// Create requests don't have the resolved_at value.
-// For this interface we'll return an empty time value.
-func (c CreateIssueRequest) GetResolvedAt() time.Time { return time.Time{} }
+func (c CreateIssueRequest) GetResolvedAt() time.Time {
+	// CREATE requests don't have the resolved_at value.
+	// For this interface we'll return an empty time value.
+	return time.Time{}
+}
 
 func (u UpdateIssueRequest) GetTitle() string               { return u.Title }
 func (u UpdateIssueRequest) GetDescription() string         { return u.Description }
@@ -78,6 +109,6 @@ func (u UpdateIssueRequest) GetSeverity() models.Severity   { return u.Severity 
 func (u UpdateIssueRequest) GetIssueType() models.IssueType { return u.IssueType }
 func (u UpdateIssueRequest) GetState() models.IssueState    { return u.State }
 func (u UpdateIssueRequest) GetLinks() []CreateLinkRequest  { return u.Links }
-func (u UpdateIssueRequest) GetScope() ScopeReqBody         { return u.Scope }
+func (u UpdateIssueRequest) GetScope() ScopePayload         { return u.Scope }
 func (u UpdateIssueRequest) GetNamespace() string           { return u.Namespace }
 func (u UpdateIssueRequest) GetResolvedAt() time.Time       { return u.ResolvedAt }
